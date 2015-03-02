8c940b138dbd9079740b3ae945425fa65345a8cd
lekalina <lekalina@lekalina-Aspire-1830T>
2015-03-01 23:43:44 -0500

    initial push to repos

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+            <title> Lekalina's HTML Webpage </title>
+	</head>
+        
+        <body> <!--style="background-color: lightblue"-->
+            
+            <h1>Lek's Webpage</h1>
+            <h2 title="Lek's Webpage" style="color: magenta">Welcome to my webpage</h2>
+            <p>
+                <br>Wanna know more about me? Visit one of my websites.<br>
+            <a href="http://www.lekalina.tumblr.com">Lekalina's Tumblr Page</a><br>
+            <a href="http://www.twitter.com/lekalina">Lekalina's Twitter Page</a>
+            </p>
+            <p>Wanna get away?<br></p>
+            <a href="http://www.kayak.com">
+            <img src="palmtree.jpg" style="width:400px;height:300px">
+            </a>
+            <p>To open a file, select:</p>
+            <p><kbd>File | Open...</kbd></p>
+            <p>Coding Example:</p>
+            <code>
+                <pre>
+                var person = {
+                firstName: "John",
+                lastName: "Doe",
+                age:50,
+                eyeColor: "blue"
+                }
+                </pre>
+            </code>
+            
+	</body>
+</html>
